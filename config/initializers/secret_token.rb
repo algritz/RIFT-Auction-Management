cecacd5c0678d655b145d2b6a63cf2a0e9931cae
David Cloutier <algritz@videotron.ca>
2011-07-08 14:21:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RIFTAuctionManagement::Application.config.secret_token = 'b0b0bd8efb41f865dee6cb33f2d3817d782e46739713b69a41d208f109a7342095ced48233ee0deb7fb38466212dc4869632d9153d993289f32de5b6a00f66f1'
