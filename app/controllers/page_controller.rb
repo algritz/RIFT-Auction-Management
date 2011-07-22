715ff3b71f182a8fafa815641b9a1443e406567b
David Cloutier <algritz@videotron.ca>
2011-07-22 14:06:57 -0400

    added more flexibility in items_to_craft calculations

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,9 +1,10 @@
 class PageController < ApplicationController
   def items_to_craft
     item_ids = Item.find(:all, :conditions => "to_list = 't'", :select => "id")
+    sold = ListingStatus.find(:all, :conditions => "description ='Sold'").first
     @out_of_stock_list = []
     item_ids.each do |ids|
-      active_autions = SalesListing.count(ids.id, :conditions => "item_id = #{ids.id} and listing_status_id <> 3")
+      active_autions = SalesListing.count(ids.id, :conditions => "item_id = #{ids.id} and listing_status_id <> '#{sold.id}'")
       if active_autions == 0 then
       @out_of_stock_list << ids.id
       end
