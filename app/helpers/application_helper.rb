d9b84381f59582132c26cb20e6f77cafc8ac9093
David Cloutier <algritz@videotron.ca>
2011-07-22 23:02:43 -0400

    added automated calculations on listing creation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,25 +46,27 @@ module ApplicationHelper
   end
 
   def calculateCraftingCost(id)
-    if Item.find(id).is_crafted then
-      if CraftedItem.where("crafted_item_generated_id = #{id}").exists? then
-        crafting_materials = CraftedItem.find(:all, :conditions => "crafted_item_generated_id = #{id}")
-        cost = 0
-        crafting_materials.each do |materials|
-          material_cost = calculateCraftingCost(materials.component_item_id)
-          total_material_cost = (material_cost * materials.component_item_quantity)
-          if (material_cost.to_s != "no pattern defined yet for a sub-component") then
-          cost += total_material_cost
-          else
-            return "no pattern defined yet for a sub-component"
+    if id != nil then
+      if Item.find(id).is_crafted then
+        if CraftedItem.where("crafted_item_generated_id = #{id}").exists? then
+          crafting_materials = CraftedItem.find(:all, :conditions => "crafted_item_generated_id = #{id}")
+          cost = 0
+          crafting_materials.each do |materials|
+            material_cost = calculateCraftingCost(materials.component_item_id)
+            total_material_cost = (material_cost * materials.component_item_quantity)
+            if (material_cost.to_s != "no pattern defined yet for a sub-component") then
+            cost += total_material_cost
+            else
+              return "no pattern defined yet for a sub-component"
+            end
           end
+        return cost
+        else
+          return "no pattern defined yet for a sub-component"
         end
-      return cost
       else
-        return "no pattern defined yet for a sub-component"
+        return calculateBuyingCost(id)
       end
-    else
-      return calculateBuyingCost(id)
     end
   end
 
@@ -111,40 +113,45 @@ module ApplicationHelper
   end
 
   def averageSalesPrice(id)
-    sold = ListingStatus.find(:all, :conditions => "description ='Sold'").first
-    sql_str = "listing_status_id = #{sold.id} and item_id = #{id} and is_undercut_price = 'f'"
-    price = SalesListing.average(:price, :conditions => sql_str)
+    if id != nil then
+      sold = ListingStatus.find(:all, :conditions => "description ='Sold'").first
+      sql_str = "listing_status_id = #{sold.id} and item_id = #{id} and is_undercut_price = 'f'"
+      price = SalesListing.average(:price, :conditions => sql_str)
     return price.to_i
+    end
   end
 
   def lastSalesPrice(id)
-    sold = ListingStatus.find(:all, :conditions => "description ='Sold'").first
+    if id != nil then
+      sold = ListingStatus.find(:all, :conditions => "description ='Sold'").first
 
-    sql_str = "listing_status_id = #{sold.id} and item_id = #{id} and is_undercut_price = 'f'"
-    sold = SalesListing.find(:all, :conditions => sql_str).last
-    if sold != nil then
-      sql_str_sold = sold.id
-      price = SalesListing.find(sql_str_sold).price
-    return price
+      sql_str = "listing_status_id = #{sold.id} and item_id = #{id} and is_undercut_price = 'f'"
+      sold = SalesListing.find(:all, :conditions => sql_str).last
+      if sold != nil then
+        sql_str_sold = sold.id
+        price = SalesListing.find(sql_str_sold).price
+      return price
+      end
     end
-
   end
 
   def lastListings(id)
-    sold = ListingStatus.find(:all, :conditions => "description ='Sold'").first
-    # should get last sale date in order to limit records, since "limit" doesn't work on '.count' relations
-    sql_str = "listing_status_id = #{sold.id} and item_id = #{id} and is_undercut_price = 'f'"
-    last_sold = SalesListing.find(:all, :conditions => sql_str, :order => "updated_at desc").first
-    if last_sold != nil then
-      lastListings = SalesListing.count(:all, :conditions => "item_id = #{id} and updated_at >= '#{last_sold.updated_at}' and is_undercut_price = 'f'", :group => 'listing_status_id')
-    else
-      lastListings = SalesListing.count(:all, :conditions => "item_id = #{id} and is_undercut_price = 'f'", :group => 'listing_status_id')
-    end
-    lastListings_per_status = []
-    lastListings.each do |status, value|
-      lastListings_per_status << "#{getListingStatusDescription(status)} : #{value} <br />"
-    end
+    if id != nil then
+      sold = ListingStatus.find(:all, :conditions => "description ='Sold'").first
+      # should get last sale date in order to limit records, since "limit" doesn't work on '.count' relations
+      sql_str = "listing_status_id = #{sold.id} and item_id = #{id} and is_undercut_price = 'f'"
+      last_sold = SalesListing.find(:all, :conditions => sql_str, :order => "updated_at desc").first
+      if last_sold != nil then
+        lastListings = SalesListing.count(:all, :conditions => "item_id = #{id} and updated_at >= '#{last_sold.updated_at}' and is_undercut_price = 'f'", :group => 'listing_status_id')
+      else
+        lastListings = SalesListing.count(:all, :conditions => "item_id = #{id} and is_undercut_price = 'f'", :group => 'listing_status_id')
+      end
+      lastListings_per_status = []
+      lastListings.each do |status, value|
+        lastListings_per_status << "#{getListingStatusDescription(status)} : #{value} <br />"
+      end
     return lastListings_per_status
+    end
   end
 
 end
--- app/helpers/sales_listings_helper.rb
+++ app/helpers/sales_listings_helper.rb
@@ -1,10 +1,47 @@
 module SalesListingsHelper
-  def getDefaultStacksize
-    # p params
+  def getDefaultItemSelected
+    if params[:item_id] != nil then
+      item_id = params[:item_id]
+    else
+    @sales_listing.item_id
+    end
   end
 
-  def returnStackSize
-    #p params
+  def getDefaultStacksize
+    if params[:item_id] != nil then
+    1
+    else
+    @sales_listing.stacksize
+    end
+  end
+
+  def getDefaultPricing
+    if params[:item_id] != nil then
+      item_id = params[:item_id]
+      lastSalesPrice(item_id)
+    else
+    @sales_listing.price
+    end
+  end
+
+  def getDefaultStatus
+    if params[:item_id] != nil then
+      listing_status = ListingStatus.find(:all, :conditions => "description = 'Ongoing'").first
+    listing_status.id
+    else
+    @sales_listing.listing_status_id
+    end
+  end
+
+  def getDepositCost
+    if params[:item_id] != nil then
+      last_listing = SalesListing.find(:all, :conditions => "item_id = #{params[:item_id]}").last
+      if last_listing != nil then
+      last_listing.deposit_cost
+      end
+    else
+    @sales_listing.deposit_cost
+    end
   end
 
 end
