88d1b48fb3082f47a4413368ff7633b52c1a903f
David Cloutier <algritz@videotron.ca>
2011-07-24 21:27:32 -0400

    added stacksize calculation on crafted item creation

--- app/helpers/crafted_items_helper.rb
+++ app/helpers/crafted_items_helper.rb
@@ -7,4 +7,13 @@ module CraftedItemsHelper
     @crafted_item.crafted_item_generated_id
     end
   end
+
+  def getDefaultItemStacksize
+    if params[:item_id] != nil then
+      crafted_item_generated_id = params[:item_id]
+      crafted_item_stacksize = CraftedItem.find(:last, :conditions => "crafted_item_generated_id = #{crafted_item_generated_id}", :select => 'crafted_item_stacksize').crafted_item_stacksize
+    else
+    @crafted_item.crafted_item_stacksize
+    end
+  end
 end
