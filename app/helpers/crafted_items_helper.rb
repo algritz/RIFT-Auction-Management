970059ed8170303199130fc4b7700814c994f04e
David Cloutier <algritz@videotron.ca>
2011-07-24 21:17:22 -0400

    added auto complete to crafted item (issue#4)

--- app/helpers/crafted_items_helper.rb
+++ app/helpers/crafted_items_helper.rb
@@ -1,2 +1,10 @@
 module CraftedItemsHelper
+  def getDefaultItemValue
+    if params[:item_id] != nil then
+      crafted_item_generated_id = params[:item_id]
+    crafted_item_generated_id
+    else
+    @crafted_item.crafted_item_generated_id
+    end
+  end
 end
