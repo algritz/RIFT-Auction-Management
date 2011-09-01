ca492d93caf96de5fa24a13e1899e2ebc256baf0
David Cloutier <algritz@videotron.ca>
2011-09-01 15:00:34 -0400

    fixed pgsql specific query with a date

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -89,7 +89,7 @@ class PageController < ApplicationController
 
     @salesListing = SalesListing.joins("left join listing_statuses on sales_listings.listing_status_id = listing_statuses.id").sum(:price,
     :conditions => (["sales_listings.user_id = ? and listing_statuses.description = ?", current_user.id, "Sold"]),
-    :group => ("strftime('%Y-%m-%d', sales_listings.updated_at)"))
+    :group => ("DATE(sales_listings.updated_at)"))
 
   end
 
