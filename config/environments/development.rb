61abbeef07bad7cebd99876dc63b53ffa53e906d
David Cloutier <algritz@videotron.ca>
2011-10-27 16:06:35 -0400

    added more config related to rails 3.1.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,6 @@ RIFTAuctionManagement::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  #config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -23,11 +22,12 @@ RIFTAuctionManagement::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  #config.middleware.use ::ExceptionNotifier,
-  #:email_prefix => "Rift-Auction-Management Errors: ",
-  #:sender_address => %w{rift-ah@invalidemail.com},
-  #:exception_recipients => %w{algritz@videotron.ca}
-
+  # Do not compress assets
+  config.assets.compress = false
+  
+  # Expands the lines which load the assets
+  config.assets.debug = true
+  
   ## splits the development log in 25 megabytes chunks, discards older logs
   config.logger = Logger.new(Rails.root.join("log",Rails.env + ".log"),3,25*1024*1024)
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,6 @@ RIFTAuctionManagement::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_controller.consider_all_requests_local = false
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
@@ -48,5 +47,14 @@ RIFTAuctionManagement::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
 end
